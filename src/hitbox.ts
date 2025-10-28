8315b23fbb0112bd44c7226c25f3925bd25aea7b
dotundrscr <83116565+dotundrscr@users.noreply.github.com>
2025-10-28 20:51:18 +0300

    replace @param annotations with @property

--- src/hitbox.ts
+++ src/hitbox.ts
@@ -4,8 +4,8 @@
 /**
  * Base hitbox class.
  *
- * @param attachTo BasePart to attach the hitbox to.
- * @param attachOffset Hitbox's offset relative to the origin of `attachTo`
+ * @property attachTo BasePart to attach the hitbox to.
+ * @property attachOffset Hitbox's offset relative to the origin of `attachTo`
  */
 export abstract class Hitbox {
   attachTo: BasePart;
