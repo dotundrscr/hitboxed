38f125a4c3cbed075a609e65e768de4d29c5d38d
dotundrscr <83116565+dotundrscr@users.noreply.github.com>
2025-10-28 21:02:43 +0300

    add properties for entity hitbox
    the hitbox doesn't have to have a lot of properties, since its just a
    part that is attached to the player

--- src/hitbox.ts
+++ src/hitbox.ts
@@ -22,7 +22,20 @@ export abstract class Hitbox {
  * Should be used for Humanoids or parts that are supposed to take damage.
  */
 export class EntityHitbox extends Hitbox {
-  constructor(attachTo: BasePart, attachOffset: Vector3) {
+  protected hitboxInstance: Part = new Instance("Part");
+  protected position: Vector3 = new Vector3(0, 0, 0);
+
+  size: Vector3;
+  shape: Enum.PartType;
+
+  constructor(
+    attachTo: BasePart,
+    attachOffset: Vector3,
+    size: Vector3,
+    shape: Enum.PartType,
+  ) {
     super(attachTo, attachOffset);
+    this.size = size;
+    this.shape = shape;
   }
 }
