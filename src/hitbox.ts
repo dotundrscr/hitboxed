45e837d850f60b321b72fa61becc066b574a676e
dotundrscr <83116565+dotundrscr@users.noreply.github.com>
2025-10-29 19:05:46 +0300

    implement entity hitboxes

--- src/hitbox.ts
+++ src/hitbox.ts
@@ -24,6 +24,8 @@ export abstract class Hitbox {
 export class EntityHitbox extends Hitbox {
   protected hitboxInstance: Part = new Instance("Part");
   protected position: Vector3 = new Vector3(0, 0, 0);
+  protected attachmentInstance: Attachment = new Instance("Attachment");
+  protected constraint: RigidConstraint = new Instance("RigidConstraint");
 
   size: Vector3;
   shape: Enum.PartType;
@@ -37,5 +39,78 @@ export class EntityHitbox extends Hitbox {
     super(attachTo, attachOffset);
     this.size = size;
     this.shape = shape;
+
+    this._attach();
+  }
+
+  /**
+   * Attach the Hitbox instance to `attachTo`.
+   *
+   * @protected DO NOT USE THIS FUNCTION DIRECTLY. This is a protected function in TypeScript, but still can be called in Luau.
+   */
+  protected _attach() {
+    const hitboxInstance = this._createInstance();
+
+    if (!this.attachTo) {
+      error("hitboxed: attachTo of a Hitbox is nil");
+    }
+
+    if (!this.attachTo.IsA("BasePart")) {
+      error(
+        `hitboxed: attachTo of a Hitbox is not a BasePart (attachTo = ${this.attachTo})`,
+      );
+    }
+
+    hitboxInstance.Parent = this.attachTo;
+
+    const partAttachment = new Instance("Attachment");
+    partAttachment.Name = "hitboxedAttachment";
+    partAttachment.Parent = this.attachTo;
+
+    const hitboxAttachment = new Instance("Attachment");
+    hitboxAttachment.Name = "hitboxedAttachment";
+    hitboxAttachment.Position = this.attachOffset;
+    hitboxAttachment.Parent = hitboxInstance;
+
+    const constraint = new Instance("RigidConstraint");
+    constraint.Name = "hitboxedConstraint";
+    constraint.Attachment0 = partAttachment;
+    constraint.Attachment1 = hitboxAttachment;
+    constraint.Parent = this.attachTo;
+
+    this.hitboxInstance = hitboxInstance;
+    this.attachmentInstance = partAttachment;
+    this.constraint = constraint;
+  }
+
+  /**
+   * Create a new instance of the hitbox.
+   *
+   * @protected DO NOT USE THIS FUNCTION DIRECTLY. This is a protected function in TypeScript, but still can be called in Luau.
+   */
+  protected _createInstance(): Part {
+    const hitboxInstance = new Instance("Part");
+
+    hitboxInstance.Name = "hitboxedEntity";
+
+    hitboxInstance.Size = this.size;
+    hitboxInstance.Shape = this.shape;
+
+    hitboxInstance.Material = Enum.Material.SmoothPlastic;
+    hitboxInstance.Color = new Color3(1, 0.69, 0);
+    hitboxInstance.Transparency = 0.5;
+    hitboxInstance.LocalTransparencyModifier = 1;
+
+    hitboxInstance.AddTag("hitboxed-entity");
+
+    return hitboxInstance;
+  }
+  /**
+   * Destroy the hitbox and it's attachment
+   */
+  destroy() {
+    this.hitboxInstance.Destroy();
+    this.attachmentInstance.Destroy();
+    this.constraint.Destroy();
   }
 }
