600db1b213c10b8278c1215e52caebb74ce302fa
dotundrscr <83116565+dotundrscr@users.noreply.github.com>
2025-10-30 16:56:12 +0300

    setup hitboxed collision group

---
+++ src/setup/collisiongroups.server.ts
@@ -0,0 +1,20 @@
+// Copyright (c) 2025 dotundrscr. Licensed under BSD 3-Clause "New" or "Revised" License.
+// For full terms, refer to the LICENSE file in the repository, or the SPDX License List.
+
+import { PhysicsService } from "@rbxts/services";
+
+print("hitboxed: setting up collision groups...");
+
+const startTime = os.clock();
+
+PhysicsService.RegisterCollisionGroup("hitboxed");
+
+const collisionGroups = PhysicsService.GetRegisteredCollisionGroups();
+
+for (const group of collisionGroups) {
+	if (group.name != "hitboxed") {
+		PhysicsService.CollisionGroupSetCollidable("hitboxed", group.name, false);
+	}
+}
+
+print(`hitboxed: done setting up collisiong roups (${os.clock() - startTime})`);
