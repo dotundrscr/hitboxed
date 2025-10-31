77357ffb870318f844925b9eecb63c540b05e383
dotundrscr <83116565+dotundrscr@users.noreply.github.com>
2025-10-31 20:03:02 +0300

    implement hitbox deletion and searching

--- src/server/storage.ts
+++ src/server/storage.ts
@@ -6,6 +6,14 @@ export function StoreHitbox(hitbox: Hitbox) {
   ActiveHitboxes.set(hitbox.getUuid(), hitbox);
 }
 
+export function DeleteHitbox(uuid: string) {
+  ActiveHitboxes.delete(uuid);
+}
+
 export function GetHitboxes(): Map<string, Hitbox> {
   return ActiveHitboxes;
+}
+
+export function FindHitbox(uuid: string): Hitbox | undefined {
+  return ActiveHitboxes.get(uuid);
 }
